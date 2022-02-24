98911b14bf0f322424c4d8b95788f6744cb45df4
Summer <511391805@qq.com>
2022-02-24 23:58:57 +0800

    集成gin

--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	fmt.Println("Hello 世界")
+	// new 一个Gin Engine 实例
+	r := gin.New()
+	// 注册中间件
+	r.Use(gin.Logger(), gin.Recovery())
+	// 注册一个路由
+	r.GET("/", func(c *gin.Context) {
+		// 以JSON格式响应
+		c.JSON(http.StatusOK, gin.H{
+			"Hello": "World!",
+		})
+	})
+	// 运行服务
+	r.Run(":8000")
 }
