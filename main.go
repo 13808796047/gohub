191d24c221abc622ae9ed9eca21863873db84a10
Summer <511391805@qq.com>
2022-02-25 00:18:19 +0800

    初始化路由

--- main.go
+++ main.go
@@ -1,23 +1,21 @@
 package main
 
 import (
-	"net/http"
+	"fmt"
+	"gohub/bootstrap"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	// new 一个Gin Engine 实例
-	r := gin.New()
-	// 注册中间件
-	r.Use(gin.Logger(), gin.Recovery())
-	// 注册一个路由
-	r.GET("/", func(c *gin.Context) {
-		// 以JSON格式响应
-		c.JSON(http.StatusOK, gin.H{
-			"Hello": "World!",
-		})
-	})
+	router := gin.New()
+	// 初始化路由绑定
+	bootstrap.SetupRoute(router)
 	// 运行服务
-	r.Run(":8000")
+	err := router.Run(":3000")
+	if err != nil {
+		// 错误处理，端口被占用了或者其他错误
+		fmt.Println(err.Error())
+	}
 }
---
+++ routes/api.go
@@ -0,0 +1,22 @@
+package routes
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+// RegisterAPIRoutes 注册网页相关路由
+func RegisterAPIRoutes(r *gin.Engine) {
+	// 测试一个V1的路由组，我们所有的v1版本的路由将存放在这里
+	v1 := r.Group("/v1")
+	{
+		// 注册一个路由
+		v1.GET("/", func(c *gin.Context) {
+			// 以JSON格式响应
+			c.JSON(http.StatusOK, gin.H{
+				"Hello": "World",
+			})
+		})
+	}
+}
