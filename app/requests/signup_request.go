d6a543df04008a4b0bc224cb21fbf6f0676867d0
Summer <511391805@qq.com>
2022-02-26 22:15:20 +0800

    Email 是否已注册接口

--- app/requests/signup_request.go
+++ app/requests/signup_request.go
@@ -30,3 +30,32 @@ func ValidateSignupPhoneExist(data interface{}, c *gin.Context) map[string][]str
 	}
 	return govalidator.New(opts).ValidateStruct()
 }
+
+type SignupEmailExistRequest struct {
+	Email string `json:"email,omitempty" valid:"email"`
+}
+
+func ValidateSignupEamilExist(data interface{}, c *gin.Context) map[string][]string {
+	// 自定义验证规则
+	rules := govalidator.MapData{
+		"email": []string{"required", "min:4", "max:30", "email"},
+	}
+	// 自定义验证出错时的提示
+	messages := govalidator.MapData{
+		"email": []string{
+			"required:Email为必填项",
+			"min:Email长度需大于4",
+			"max:Email长度需小于30",
+			"email:Email格式不正确，请提供有效的邮箱地址",
+		},
+	}
+	// 配置初始化
+	opts := govalidator.Options{
+		Data:          data,
+		Rules:         rules,
+		TagIdentifier: "valid", // 模型中的Struct标签标识符
+		Messages:      messages,
+	}
+	// 开始验证
+	return govalidator.New(opts).ValidateStruct()
+}
