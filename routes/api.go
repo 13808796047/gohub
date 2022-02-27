06635a4c80c5c23ec293fba26ba9cf6fee806229
Summer <511391805@qq.com>
2022-02-27 21:14:15 +0800

    手机号+短信验证码注册

--- routes/api.go
+++ routes/api.go
@@ -18,6 +18,7 @@ func RegisterAPIRoutes(r *gin.Engine) {
 			authGroup.POST("signup/phone/exist", suc.IsPhoneExist)
 			// 判断Email是否已注册
 			authGroup.POST("/signup/email/exist", suc.IsEmailExist)
+			authGroup.POST("/signup/using-phone", suc.SignupUsingPhone)
 			// 发送验证码
 			vcc := new(auth.VerifyCodeController)
 			// 图片验证码，需要加限流
